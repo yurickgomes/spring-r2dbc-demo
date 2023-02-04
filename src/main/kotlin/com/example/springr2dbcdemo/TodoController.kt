4deb7857e2a9aece540fb98f7933df165af85c75
yurickgomes <tecyurick@gmail.com>
2023-02-04 20:33:25 -0300

    Initial configuration

--- src/main/kotlin/com/example/springr2dbcdemo/TodoController.kt
+++ src/main/kotlin/com/example/springr2dbcdemo/TodoController.kt
@@ -4,7 +4,9 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -17,7 +19,7 @@ class TodoController(
     private val todoRepository: TodoRepository
 ) {
     @GetMapping
-    suspend fun getAll(): Flow<TodoDto> {
+    fun getAll(): Flow<TodoDto> {
         return todoRepository.findAll().map {
             TodoDto(
                 id = it.id,
@@ -33,6 +35,28 @@ class TodoController(
         val newTodoToBePersisted = Todo(
             description = newTodoDtoBody.description,
             completed = newTodoDtoBody.completed,
+        )
+        todoRepository.save(newTodoToBePersisted)
+    }
+
+    @GetMapping("/{id}")
+    suspend fun getById(@PathVariable id: String): TodoDto? {
+        val foundTodo = todoRepository.findById(id) ?: return null
+
+        return TodoDto(
+            id = foundTodo.id,
+            description = foundTodo.description,
+            completed = foundTodo.completed,
+        )
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    suspend fun update(@PathVariable id: String, @RequestBody todoToBeUpdated: TodoDto) {
+        val newTodoToBePersisted = Todo(
+            id = id,
+            description = todoToBeUpdated.description,
+            completed = todoToBeUpdated.completed,
         )
         todoRepository.save(newTodoToBePersisted)
     }
